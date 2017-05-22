a7157a37f39316fddaa5c49f254fe26fabb716cc
itodaisuke <jabropt@gmail.com>
2017-05-22 22:19:47 +0900

    :white_check_mark: Use path module

--- src/formatter.js
+++ src/formatter.js
@@ -14,7 +14,7 @@ export const format = (comment, options) => {
     // "diff_hunk": "@@ -16,33 +16,40 @@ public class Connection : IConnection...",
     const [all, firstLine] = comment.diff_hunk.match(/-(\d+)/);
     const lineNumber = parseInt(firstLine, 10) - 1;
-    return `@ ${options.projectRoot}${comment.path}:${lineNumber + comment.original_position}:1
+    return `@ ${path.join(options.projectRoot, comment.path)}:${lineNumber + comment.original_position}:1
 ★ ${comment.html_url.replace(/#.*$/, '#pullrequestreview-')}${comment.pull_request_review_id}
 ${wrapQuote(comment.body.trim())}`
 };
