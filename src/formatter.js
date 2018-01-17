7a1d4fb826da51fb9a40f499c7e3de42aea8f6d5
josjos7 <jos2571@gmail.com>
2018-01-18 00:04:11 +0900

    Implement json formatter

--- src/formatter.js
+++ src/formatter.js
@@ -5,17 +5,57 @@ const wrapQuote = (body) => {
     return body;
 };
 
+const rawFormatter = (comment, options) => {
+    // "diff_hunk": "@@ -16,33 +16,40 @@ public class Connection : IConnection...",
+    const [all, firstLine] = comment.diff_hunk.match(/-(\d+)/);
+    const lineNumber = parseInt(firstLine, 10) - 1 + comment.original_position;
+    const filePath = path.join(options.projectRoot, comment.path);
+    const reviewURL = `${comment.html_url.replace(/#.*$/, '#pullrequestreview-')}${comment.pull_request_review_id}`;
+    const body = wrapQuote(comment.body.trim());
+    return {
+        file_path: filePath,
+        line_number: lineNumber,
+        review_url: reviewURL,
+        body: body,
+    }
+}
+
 /**
  * @param {object} comment
  * @param {object} options
  * @returns {string}
  * @see https://developer.github.com/v3/pulls/comments/#list-comments-on-a-pull-request
  */
-export const format = (comment, options) => {
-    // "diff_hunk": "@@ -16,33 +16,40 @@ public class Connection : IConnection...",
-    const [all, firstLine] = comment.diff_hunk.match(/-(\d+)/);
-    const lineNumber = parseInt(firstLine, 10) - 1;
-    return `@ ${path.join(options.projectRoot, comment.path)}:${lineNumber + comment.original_position}:1
-★ ${comment.html_url.replace(/#.*$/, '#pullrequestreview-')}${comment.pull_request_review_id}
-${wrapQuote(comment.body.trim())}`
+const jsonFormatter = (comments, options) => {
+    const formatData = comments.data.map(data => {
+        return rawJsonFormatter(data, options);
+    });
+    return JSON.stringify(formatData, null, '  ');
+}
+
+/**
+ * @param {object} comments
+ * @param {object} options
+ * @returns {string}
+ * @see https://developer.github.com/v3/pulls/comments/#list-comments-on-a-pull-request
+ */
+const defaultFormatter = (comments, options) => {
+    const formatData = comments.data.map(data => {
+        const js = rawJsonFormatter(data, options);
+        return `@ ${js.file_path}:${js.line_number}:1
+★ ${js.review_url}
+${js.body}`
+    });
+    return formatData.join("\n----\n");
 };
+
+/**
+ * @param {string} formatFlag
+ * @returns {object}
+ */
+export const getFormatter = (formatFlag) => {
+    if (formatFlag === 'json') {
+        return jsonFormatter;
+    }
+    return defaultFormatter;
+}
